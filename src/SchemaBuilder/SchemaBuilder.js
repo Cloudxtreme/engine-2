d2634fd606b0750823104f45c68c264084bcdfe2
fugufish <jarod@solidalchemy.com>
2018-04-12 10:54:26 -0600

    remove js compile files

--- src/SchemaBuilder/SchemaBuilder.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class SchemaBuilder {
-    schema() {
-        throw new Error('Not Implemented');
-    }
-}
-exports.SchemaBuilder = SchemaBuilder;
-//# sourceMappingURL=SchemaBuilder.js.map
