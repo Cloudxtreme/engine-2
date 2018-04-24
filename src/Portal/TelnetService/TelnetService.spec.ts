c88f253ef70ba8f8c0374e8d57f85ff4f25ef9ee
fugufish <jarod@solidalchemy.com>
2018-04-24 09:50:03 -0600

    remove invalid reference

--- src/Portal/TelnetService/TelnetService.spec.ts
+++ src/Portal/TelnetService/TelnetService.spec.ts
@@ -1,7 +1,6 @@
 import {Service, ServiceBroker} from 'moleculer';
 import {Server} from 'net';
 
-import {IPortalConfig} from '../Broker/Broker';
 import {TelnetService} from './TelnetService';
 
 jest.mock('net');
